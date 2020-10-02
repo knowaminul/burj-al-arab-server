b9733c02339e91fb32979930f9ea647f36cf8378
falvee <knowaminul@gmail.com>
2020-10-03 00:09:06 +0600

    updated

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.get('/', (req, res) =>{
 })
 
 const MongoClient = require('mongodb').MongoClient;
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shjfg.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.shjfg.mongodb.net:27017,cluster0-shard-00-01.shjfg.mongodb.net:27017,cluster0-shard-00-02.shjfg.mongodb.net:27017/burjAlArab?ssl=true&replicaSet=atlas-zplt2j-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shjfg.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+//const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.shjfg.mongodb.net:27017,cluster0-shard-00-01.shjfg.mongodb.net:27017,cluster0-shard-00-02.shjfg.mongodb.net:27017/burjAlArab?ssl=true&replicaSet=atlas-zplt2j-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
